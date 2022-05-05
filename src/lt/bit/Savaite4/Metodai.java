4ab9167f8fc0f251f05aa36336260ae2335a43ee
Dziugas Molis <molisdziugas97@gmail.com>
2022-05-05 18:06:23 +0300

    10 paskaita

---
+++ src/lt/bit/Savaite4/Metodai.java
@@ -0,0 +1,54 @@
+package lt.bit.Savaite4;
+
+public class Metodai {
+    public static void main(String[] args) {
+        // 1
+        int savaitesDiena1 = 3;
+        int savaitesDiena2 = 9;
+
+        // 2
+        int pazymys1 = 4;
+        int pazymys2 = 8;
+
+        // 3
+        double kaina1 = 297.42;
+        double kaina2 = 100;
+
+        // 4
+        int amzius1 = 18;
+        int amzius2 = 13;
+
+        // 5 ir 6
+        String asmensKodas = "37702071899";
+
+        // 7
+
+        // 8
+
+        // 9
+    }
+
+    // 1. Metodas kuriam perdavus skaiciu atspausdina savaite diena pvz. 1 = Pirmadienis
+
+    // 2. Funkcija kuri patikrina ar pazymys yra teigiamas 4 < x
+
+    // 3. Funkcija kuriai perdavus kaina grazina kaina su PVM 21%
+
+    // 4. Funkcija kuri patikrina ar zmogus yra pilnametis
+
+    // 5. Funkcija kuri pagal asmens koda patikrina ar zmogus yra vyras
+
+    // 6. Funkcija kuri pagal asmens koda patikrina ar zmogus yra moteris
+
+    // 7. Funkcija kuri grazina bet koki skaiciu
+
+    // 8. Funkcija kuri grazina bet koki skaiciu tarp 0-9
+
+    // 9. Funkcija kuri sugeneruoja slaptazodi pagal nustatytus parametrus
+    // kokius simbolius ideti i slaptazodi ir kokio ilgio jis turi buti
+    // int ilgis, boolean skaiciai, boolean simboliai, boolean raides
+    // Jeigu skaiciai = true ideti skaicius
+    // Jeigu skaiciai ir simboliai = true ideti ir skaicius ir simboliai
+
+
+}
